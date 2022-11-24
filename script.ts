c24f1a1397d7ef641d6e8db9e1c1bf54a501af49
shomrkm <shochan1985@gmail.com>
2022-11-24 09:12:44 +0900

    feat: Modify test script.

--- script.ts
+++ script.ts
@@ -2,7 +2,28 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  const users = await prisma.user.findMany();
+  // await prisma.user.deleteMany();
+  // const user = await prisma.user.create({
+  //   data: {
+  //     name: 'Hitomi',
+  //     email: 'Hitomi@test.com',
+  //     age: 33,
+  //     isAdmin: false,
+  //     userPreference: {
+  //       create: {
+  //         emailUpdates: false,
+  //       },
+  //     },
+  //   },
+  //   include: {
+  //     userPreference: true,
+  //   },
+  // });
+
+  const users = await prisma.user.findMany({
+    where: { name: 'Shotaro' },
+  });
+
   console.log(users);
 }
 
