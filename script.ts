8254b1a536deaa4cfc1fe70519fad15ed848e97d
shomrkm <shochan1985@gmail.com>
2022-11-26 16:00:18 +0900

    feat: Update a test script.

--- script.ts
+++ script.ts
@@ -2,28 +2,34 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  // await prisma.user.deleteMany();
-  // const user = await prisma.user.create({
-  //   data: {
-  //     name: 'Hitomi',
-  //     email: 'Hitomi@test.com',
-  //     age: 33,
-  //     isAdmin: false,
-  //     userPreference: {
-  //       create: {
-  //         emailUpdates: false,
-  //       },
-  //     },
-  //   },
-  //   include: {
-  //     userPreference: true,
-  //   },
-  // });
-
-  const users = await prisma.user.findMany({
-    where: { name: 'Shotaro' },
+  await prisma.user.deleteMany();
+  await prisma.user.createMany({
+    data: [
+      { name: 'Shotaro', age: 10, email: 'Shotaro@test1.com', isAdmin: true },
+      { name: 'Shotaro', age: 20, email: 'Shotaro@test2.com', isAdmin: false },
+      { name: 'Shotaro', age: 30, email: 'Shotaro@test3.com', isAdmin: true },
+      { name: 'Hitomi', age: 30, email: 'Hitomi@test1.com', isAdmin: false },
+    ],
   });
-
+  const users = await prisma.user.findMany({
+    where: {
+      OR: [
+        {
+          name: 'Shotaro',
+          age: { lt: 25 },
+          isAdmin: true,
+        },
+        {
+          email: { contains: 'Hitomi' },
+        },
+      ],
+    },
+    select: {
+      name: true,
+      email: true,
+      userPreference: { select: { id: true } },
+    },
+  });
   console.log(users);
 }
 
