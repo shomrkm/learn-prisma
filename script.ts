f0cc76829f4e81208985987a68db8b14a3830281
shomrkm <shochan1985@gmail.com>
2023-02-10 09:37:46 +0900

    feat: Update schema.prisma.

--- script.ts
+++ script.ts
@@ -1,25 +1,43 @@
 import { PrismaClient, Place } from '@prisma/client';
+import { create } from 'domain';
+import { title } from 'process';
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.place.deleteMany();
+  await prisma.user.deleteMany();
   await prisma.task.deleteMany();
+  await prisma.place.deleteMany();
 
-  await prisma.place.createMany({
-    data: [
-      { body: 'test1', latitude: 39.6961687, longitude: 141.1232217 },
-      { body: 'test2', latitude: 39.700226, longitude: 141.1388751 },
-      { body: 'test3', latitude: 39.6975349, longitude: 141.1440678 },
-    ],
+  const user = await prisma.user.create({
+    data: {
+      name: 'Shotaro Murakami',
+      email: 'shotaro@test.com',
+      createTask: {
+        create: [
+          {title: 'Shotaro\'s Task 1'},
+          {title: 'Shotaro\'s Task 2'},
+        ]
+      }
+    },
   });
-  await prisma.task.create({ data: { title: 'test' } });
+  await prisma.task.create({ data: { title: 'Task with no user' } });
 
-  // Obtain data within 500 meters from a specified position.
-  const longitude = 141.1442678;
-  const latitude = 39.6975649;
-  const result =
-    await prisma.$queryRaw<Place>`SELECT id, body FROM "Place" WHERE ST_DWithin(ST_MakePoint(longitude, latitude), ST_MakePoint(${longitude}, ${latitude})::geography, 500)`;
-  console.log(result);
+  const tasks = await prisma.task.findMany({ 
+    where: {
+      userId: user.id,
+    },
+    select: {
+      title: true, 
+      user: true
+    },
+  });
+  console.log(tasks)
+
+  // // Obtain data within 500 meters from a specified position.
+  // const longitude = 141.1442678;
+  // const latitude = 39.6975649;
+  // const result =
+  //   await prisma.$queryRaw<Place>`SELECT id, body FROM "Place" WHERE ST_DWithin(ST_MakePoint(longitude, latitude), ST_MakePoint(${longitude}, ${latitude})::geography, 500)`;
 }
 
 main()
